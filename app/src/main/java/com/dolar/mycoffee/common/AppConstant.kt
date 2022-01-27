d471544829c0430ddccefce33bf448fc72b5a161
Rıfat Mert Dolar <>
2022-01-27 09:49:06 +0300

    - app constant imp.

--- app/src/main/java/com/dolar/mycoffee/common/AppConstant.kt
+++ app/src/main/java/com/dolar/mycoffee/common/AppConstant.kt
@@ -4,4 +4,9 @@ package com.dolar.mycoffee.common
  * Created by R. Mert Dolar on 7.01.2022.
  */
 object AppConstant {
+
+    //Pages
+    const val SPLASH = "splash"
+    const val PRODUCT_LIST = "productlist"
+    const val PRODUCT_DETAIL_WITH_ARGUMENTS = "productdetail/{drinkName}/"
 }
